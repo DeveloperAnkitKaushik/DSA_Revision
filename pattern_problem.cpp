4f03a179604e06276b91f78fcee1cefa4315c2b3
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-13 22:07:05 +0530

    day 2

--- pattern_problem.cpp
+++ pattern_problem.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-int main(){
+
+int main()
+{
     int i = 1;
     int n = 4;
 
-    while(i<=n){
+    while (i <= n)
+    {
         int j = 1;
-        while(j<=i){
-            cout << i-j+1;
-            j+=1;
+        while (j <= i)
+        {
+            char ch = 'A' + j -1;
+            cout << ch;
+            j += 1;
         }
         cout << endl;
-        i+=1;
+        i += 1;
     }
-} 
+}
