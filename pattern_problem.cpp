b44b2651a19e70968912f762b001d066fddbf313
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-17 15:28:16 +0530

    day 3

--- pattern_problem.cpp
+++ pattern_problem.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-
+ 
 using namespace std;
-
-int main()
-{
-    int i = 1;
-    int n = 4;
-
-    while (i <= n)
-    {
-        int j = 1;
-        while (j <= i)
-        {
-            char ch = 'A' + j -1;
-            cout << ch;
-            j += 1;
+ 
+int main(){
+    int n = 2;
+    
+    for(int i = 2; i<n; i++){
+        if(n%i == 0){
+            cout << "not prime";
+            return 0;
+        }else{
+            cout << "prime";
+            return 0;
         }
-        cout << endl;
-        i += 1;
     }
 }
