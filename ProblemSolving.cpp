c72a7f2acf886ba0a8b2ff6afbd50c9d86308285
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-18 14:40:59 +0530

    day 4

---
+++ ProblemSolving.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+    int n = 123;
+    int ans = 0;
+    while(n != 0){
+        int digit = n % 10;
+
+        ans = (ans * 10) + digit;
+        n = n/10;
+    }    
+    cout << ans;
+}
