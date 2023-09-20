8f781c5daf30b331a8ba6372e235ba5370d3f129
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-20 10:38:51 +0530

    day 5

--- ProblemSolving.cpp
+++ ProblemSolving.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
  
 using namespace std;
+
+void printAray(int arr[], int size){
+    for(int i = 0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+}
+
+void swapAlternative(int arr[], int size){
+    for(int i=0;i<size;i+=2){
+        if(i+1 < size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
  
 int main(){
-    int n = 123;
-    int ans = 0;
-    while(n != 0){
-        int digit = n % 10;
+    int size = 4;
+    int arr[size] = {12,34,6,76,13};
 
-        ans = (ans * 10) + digit;
-        n = n/10;
-    }    
-    cout << ans;
+    swapAlternative(arr,size);
+    printAray(arr,size);
 }
