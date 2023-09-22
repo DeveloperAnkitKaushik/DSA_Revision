1fd18f530cecda3161c337359a8de0485bf1eb71
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-22 23:14:21 +0530

    day 7

--- ProblemSolving.cpp
+++ ProblemSolving.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
+#include <bits/stdc++.h>
  
 using namespace std;
  
 int main(){
-    int arr[] = {1,3,7,3,6,3,1};
-    int num = 10;
-    int temp;
-    int size = sizeof(arr)/sizeof(int);
-
-    for(int i = 0;i<size;i++){
-        for(int j = i+1;j<size;j++){
-            for(int k = j+1;k<size;k++){
-                temp = arr[i]+arr[j]+arr[k];
-                if(temp == num){
-                    cout << arr[i] << " " << arr[j] << " " << arr[k];
-                }
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-}   
+    int arr[] = {12,34,54,4,74,26,7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+   
+}
