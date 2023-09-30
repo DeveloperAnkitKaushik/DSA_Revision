adfafe47049582fe3b48dc3b085a22cc231b0b23
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-09-30 23:42:57 +0530

    day 13

--- stringProblems.cpp
+++ stringProblems.cpp
@@ -1,19 +1,26 @@
-#include <bits/stdc++.h> 
-#include<iostream>
-
+#include <iostream>
+ 
 using namespace std;
-
+ 
 int main(){
-    string str = "today is good day;";
-	string temp = "";
-	for(int i=0;i<str.length();i++){
-		if(str[i] == ' '){
-			temp.push_back('@');
-			temp.push_back('4');
-			temp.push_back('0');
-		}else{
-			temp.push_back(str[i]);
+	cout << "Enter the value of rows and colums: ";
+	int a,b;;
+	cin >> a >> b;
+	int arr[a][b];
+	for(int i=0;i<a;i++){
+		cout << "Enter the value for " << i+1 << " row: " <<endl;
+		for(int j=0;j<b;j++){
+			cin >> arr[i][j];
 		}
 	}
-	cout << temp;
-}
+	cout <<endl;
+	cout <<endl;
+	cout << "Your Table"<<endl;
+	for(int i=0;i<a;i++){
+		for(int j=0;j<b;j++){
+			cout << arr[i][j];
+		}
+		cout <<endl;
+	}
+	
+}	
