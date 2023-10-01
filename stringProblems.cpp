54f9cb3f9088d70a263f0a4996ffc7c819e92049
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-10-01 19:11:10 +0530

    day 15

--- stringProblems.cpp
+++ stringProblems.cpp
@@ -3,24 +3,5 @@
 using namespace std;
  
 int main(){
-	cout << "Enter the value of rows and colums: ";
-	int a,b;;
-	cin >> a >> b;
-	int arr[a][b];
-	for(int i=0;i<a;i++){
-		cout << "Enter the value for " << i+1 << " row: " <<endl;
-		for(int j=0;j<b;j++){
-			cin >> arr[i][j];
-		}
-	}
-	cout <<endl;
-	cout <<endl;
-	cout << "Your Table"<<endl;
-	for(int i=0;i<a;i++){
-		for(int j=0;j<b;j++){
-			cout << arr[i][j];
-		}
-		cout <<endl;
-	}
 	
-}	
+}
