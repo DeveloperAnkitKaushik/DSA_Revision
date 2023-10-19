ab2b287847ca78c23528b7dae7d37859d9c61099
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-10-19 17:03:04 +0530

    day 27

--- ObjectOrientedProgramming.cpp
+++ ObjectOrientedProgramming.cpp
@@ -1,34 +1,128 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-class Class1{
-    public:
-        int value_class1 = 12;
-        void print(){
-            cout << "Class1 Called"<<endl;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    ~Node()
+    {
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
         }
+        cout << "Node Deleted: " << this->data << endl;
+    }
 };
 
-class Class2: public Class1{
-    public:
-        int value_class2 = 23;
-        void print(){
-            cout << "Class2 Called " << value_class1 <<endl;
+void insertAtHead(Node *&head, int data)
+{
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtTail(Node *&tail, int data)
+{
+    Node *newNode = new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void insertAtPosition(Node *&head, Node *&tail, int position, int data)
+{
+    Node *newNode = new Node(data);
+    if (position == 1)
+    {
+        insertAtHead(head, data);
+        head = newNode;
+        return;
+    }
+    int count = 1;
+    Node *temp = head;
+    while (count < position - 1)
+    {
+        if (temp->next == NULL)
+        {
+            insertAtTail(tail, data);
+            tail = newNode;
+            return;
         }
+        temp = temp->next;
+        count++;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
 
-};
-
-class Class3: public Class2{
-    public:
-        void print(){
-            cout << "Class3 Called " << value_class2 << value_class1 <<endl;
+void deleteNode(Node *&head, Node *&tail, int position)
+{
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete (temp);
+    }
+    else
+    {
+        Node *current = head;
+        Node *prev = NULL;
+        int count = 1;
+        while (count < position)
+        {
+            if (current->next == NULL)
+            {
+                tail = prev;
+                prev->next = current->next;
+                current->next = NULL;
+                delete current;
+                return;
+            }
+            prev = current;
+            current = current->next;
+            count++;
         }
-};
+        prev->next = current->next;
+        current->next = NULL;
+        delete current;
+    }
+}
 
-int main(){
-    Class2 ch;
-    ch.print();
-    Class3 sh;
-    sh.print();
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *node1 = new Node(7);
+    Node *head = node1;
+    Node *tail = node1;
+
+    insertAtHead(head, 90);
+    insertAtTail(tail, 70);
+    insertAtPosition(head, tail, 10, 8);
+    print(head);
+    cout << endl;
+    deleteNode(head, tail, 10);
+    print(head);
+
+    cout << endl
+         << endl
+         << "Head is " << head->data << endl;
+    cout << "tail is " << tail->data << endl;
 }
