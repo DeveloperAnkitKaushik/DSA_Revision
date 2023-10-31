f6737a1b13917f75696b015351a47c198bd59f5e
Ankit Kaushik <ankitkaushik6269@gmail.com>
2023-10-31 19:51:42 +0530

    day 34

--- ObjectOrientedProgramming.cpp
+++ ObjectOrientedProgramming.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <cmath>
  
 using namespace std;
  
+int increase(int &n,int where){
+    int num = pow(10,where)+1;
+    return n+num;
+}
+
 int main(){
-    string n = "ankit";
-    cout << n[0];
-}   
+    string str = "999";
+    int n = str.length();
+    int middle = n/2;
+
+    int intStr = stoi(str);
+    int actualNumber = increase(intStr,middle);
+    str = to_string(actualNumber);
+    string str2 = str;
+    cout << "str: " <<str << endl;
+    // cout << "middle: " <<middle << endl;
+    if(n%2 == 0){
+        int i = middle-1;
+        int j = middle;
+        // cout << "even" << endl;
+        for(j;j<n;j++){
+            str[j] = str[i];
+            // cout << "i: " << i << endl;
+            // cout << "j: " << j << endl;
+            i-=1;
+        }
+    }
+    else{
+        int i = middle;
+        int j = middle;
+        // cout << "odd" << endl;
+        for(j;j<n;j++){
+            str[j] = str2[i];
+            // cout << "i: " << i << endl;
+            // cout << "j: " << j << endl;
+            i-=1;
+        }
+    }
+    cout << str;
+}
